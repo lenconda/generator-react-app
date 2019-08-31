c8f710829730772c1f14b3c0134f3f0c8e8ce441
lenconda <i@lenconda.top>
2019-09-01 00:28:40 +0800

    :sparkles: Add env.config.js to common template

---
+++ generators/app/templates/common/src/config/env.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  isDev: process.env.NODE_ENV.toLowerCase() === 'development',
+  isProduction: process.env.NODE_ENV.toLowerCase() === 'production',
+  isTest: process.env.NODE_ENV.toLowerCase() === 'test',
+  analyze: process.env.WEBPACK_ANALYZE ? process.env.WEBPACK_ANALYZE.toLowerCase() === 'true' : false
+};
